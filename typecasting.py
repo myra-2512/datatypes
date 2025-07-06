7b2fd1df9c1adf45a690a6903b2d97b05c8c3f7f
myra-2512 <abhi.back2back@gmail.com>
2025-07-06 10:51:28 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,27 @@
+name="Agrima"
+age=13
+is_student=True
+weight=35.6
+
+print("Name:", name)
+print("Data types of name is:", type(name))
+
+print("age:",age)
+print("Data types of age is:",type(age))
+
+print("is_student:",is_student)
+print("Data types of is_student is:",type(is_student))
+
+print("weight:",weight)
+print("Data types of weight is:",type(weight))
+
+print("\n After type casting...")
+age=str(age)
+print(age)
+print("Data types of age is:",type(age))
+
+weight=int(weight)
+print(weight)
+print("Data types of weight is:",type(weight))
+
+#Activity 3
